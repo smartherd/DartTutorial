8b3caf204270ac3ec7856ba37473156685f5a6ab
Smartherd <smartherd@gmail.com>
2018-06-07 16:40:44 +0530

    Define Constants using 'final' and 'const' keyword

---
+++ 4constants.dart
@@ -0,0 +1,19 @@
+
+void main() {
+
+	// final
+	final cityName = 'Mumbai';
+	//	name = 'Peter';     // Throws an error
+
+	final String countryName = 'India';
+
+	// const
+	const PI = 3.14;
+	const double gravity = 9.8;
+}
+
+class Circle {
+
+	final color = 'Red';
+	static const PI = 3.14;
+}
