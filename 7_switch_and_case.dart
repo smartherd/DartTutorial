8d49add455aff176983bb8d8a7d251f2a49dc367
Smartherd <smartherd@gmail.com>
2018-06-10 15:13:47 +0530

    Switch and Case statements

---
+++ 7_switch_and_case.dart
@@ -0,0 +1,28 @@
+
+void main() {
+
+	// Switch Case Statements: Applicable for only 'int' and 'String'
+
+	String grade = 'A';
+
+	switch (grade) {
+
+		case 'A':
+			print("Excellent grade of A");
+			break;
+
+		case 'B':
+			print("Very Good !");
+			break;
+
+		case 'C':
+			print("Good enough. But work hard");
+			break;
+
+		case 'F':
+			print("You have failed");
+			break;
+		default:
+			print("Invalid Grade");
+	}
+}
