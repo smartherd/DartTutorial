6698feff8bcce86a12d860f4ecb8cb65ae456247
Smartherd <smartherd@gmail.com>
2018-06-11 15:07:30 +0530

    Do-WHILE Loop

---
+++ 10_do_while_loop.dart
@@ -0,0 +1,17 @@
+
+void main() {
+
+	// DO-WHILE Loop
+	// WAP to find the even numbers between 1 to 10
+
+	int i = 1;
+
+	do {
+
+		if ( i % 2 == 0) {
+			print(i);
+		}
+
+		i++;
+	} while ( i <= 10);
+}
