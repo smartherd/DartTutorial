18a206c443e39d3060b284a91f1453e21e506f9e
Smartherd <smartherd@gmail.com>
2018-06-27 22:36:10 +0530

    Expressions in Function. SHORT HAND SYNTAX

---
+++ 14_function_expression.dart
@@ -0,0 +1,18 @@
+// OBJECTIVE: Expression in Function: SHORT HAND SYNTAX
+
+void main() {
+
+	findPerimeter(4, 2);
+
+	int rectArea = getArea(10, 5);
+	print("The area is $rectArea");
+}
+
+void findPerimeter(int length, int breadth) => print("The perimeter is ${2 * (length + breadth)}");
+
+int getArea(int length, int breadth) => length * breadth;
+
+
+// "=>" is known as FAT ARROW
+// "=> expression" is a SHORT HAND SYNTAX for { return expression; }
+// Example "=> length * breadth" is SHORT HAND SYNTAX for { return length * breadth; }
