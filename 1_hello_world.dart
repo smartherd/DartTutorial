78576d950e6a967e2e7a9b461fb85884d756a616
Smartherd <smartherd@gmail.com>
2018-06-29 13:59:30 +0530

    First Dart App with Comments

--- 1_hello_world.dart
+++ 1_hello_world.dart
@@ -2,7 +2,14 @@
 
 void main() {
 
-	print("Hello World");
+	// This is my first line of code
+	print("Hello World"); 			// this is another comment ....
 
-	print("This is my first program");
+	print("This is my first application");
+
+	// Performing arithematic operation
+	print(12 / 4);
+
+	// Printing out boolean value
+	print(false);
 }
