8b3f03617c2a91fe1f5ab9d185d5a754db1c0815
Smartherd <smartherd@gmail.com>
2018-07-02 14:58:19 +0530

    Inheritance with Named and Default Constructors

---
+++ 24_inheritance_with_constructors.dart
@@ -0,0 +1,46 @@
+
+// Objectives
+// 1. Inheritance with Default Constructor and Parameterised Constructor
+// 2. Inheritance with Named Constructor
+
+void main() {
+
+	var dog1 = Dog("Labrador", "Black");
+
+	print("");
+
+	var dog2 = Dog("Pug", "Brown");
+
+	print("");
+
+	var dog3 = Dog.myNamedConstructor("German Shepherd", "Black-Brown");
+}
+
+class Animal {
+
+	String color;
+
+	Animal(String color) {
+		this.color = color;
+		print("Animal class constructor");
+	}
+
+	Animal.myAnimalNamedConstrctor(String color) {
+		print("Animal class named constructor");
+	}
+}
+
+class Dog extends Animal {
+
+	String breed;
+
+	Dog(String breed, String color) : super(color) {
+		this.breed = breed;
+		print("Dog class constructor");
+	}
+
+	Dog.myNamedConstructor(String breed, String color) : super.myAnimalNamedConstrctor(color) {
+		this.breed = breed;
+		print("Dog class Named Constructor");
+	}
+}
