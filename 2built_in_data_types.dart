39398ddb38a7f532871314c907c1a28f2ef84b77
hari <hsi1995@outlook.com>
2020-10-01 09:09:25 +0530

    added some more datatypes & improved documentation

--- 2built_in_data_types.dart
+++ 2built_in_data_types.dart
@@ -1,27 +1,45 @@
-
 void main(List<String> arguments) {
+  // Numbers: int
+  int score = 23;
+  var count = 23; // It is inferred as integer automatically by compiler
+  int hexValue = 0xEADEBAEE;
+  print(score.runtimeType);
+  print(count.runtimeType);
+  print(hexValue.runtimeType);
 
-	// Numbers: int
-	int score = 23;
-	var count = 23;     // It is inferred as integer automatically by compiler
-	int hexValue = 0xEADEBAEE;
+  // Numbers: double
+  double percentage = 93.4;
+  var percent = 82.533;
+  double exponents = 1.42e5;
+  print(percentage.runtimeType);
+  print(percent.runtimeType);
+  print(exponents.runtimeType);
 
-	// Numbers: double
-	double percentage = 93.4;
-	var percent = 82.533;
-	double exponents = 1.42e5; 
+  // Strings
+  String name = "Henry";
+  var company = "Google";
+  print(name.runtimeType);
+  print(company.runtimeType);
 
-	// Strings
-	String name = "Henry";
-	var company = "Google";
+  // Boolean
+  bool isValid = true;
+  var isAlive = false;
+  print(isValid.runtimeType);
+  print(isAlive.runtimeType);
 
-	// Boolean
-	bool isValid = true;
-	var isAlive = false;
+  //List
+  var list1 = [0, 1, 2, 3];
+  List list2 = [0, 1, 2, 3];
+  var list3 = new List();
+  print(list1.runtimeType);
+  print(list2.runtimeType);
+  print(list3.runtimeType);
 
-	print(score);
-	print(exponents);
+  //Maps
+  var employee = {'name': 'Henry', 'company': 'Google', 'Branch': 'New York'};
+  print(employee.runtimeType);
 
-	// NOTE: All data types in Dart are Objects.
-	// Therefore, their initial value is by default 'null'
+  // NOTE: All data types in Dart are Objects.
+  // Therefore, their initial value is by default 'null'
+  // runtimetype represent runtime type of the object
 }
--- 4constants.dart
+++ 4constants.dart
@@ -1,19 +1,18 @@
-
 void main() {
+  // final
+  final cityName = 'Mumbai';
+  //	name = 'Peter';     // Throws an error
 
-	// final
-	final cityName = 'Mumbai';
-	//	name = 'Peter';     // Throws an error
+  final String countryName = 'India';
 
-	final String countryName = 'India';
-
-	// const
-	const PI = 3.14;
-	const double gravity = 9.8;
+  // const
+  const PI = 3.14;
+  const double gravity = 9.8;
 }
 
 class Circle {
-
-	final color = 'Red';
-	static const PI = 3.14;
+  final color = 'Red';
+  static const PI = 3.14;
 }
+
+// NOTE: const keyword represents the compile-time constant, and the final variable can be set only once.
