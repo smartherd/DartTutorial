b67702aea422b0c5391a382db5efa864a2897f17
kach4n <luizkachan@gmail.com>
2023-06-15 16:58:45 -0300

    fix some files

--- 22_inheritance.dart
+++ 22_inheritance.dart
@@ -1,49 +1,48 @@
-
 // Objectives
 // 1. Exploring Inheritance
 
 void main() {
+  var dog = Dog();
+  dog.breed = "Labrador";
+  dog.color = "Black";
+  dog.bark();
+  dog.eat();
 
-	var dog = Dog();
-	dog.breed = "Labrador";
-	dog.color = "Black";
-	dog.bark();
-	dog.eat();
+  var cat = Cat();
+  cat.color = "White";
+  cat.age = 6;
+  cat.eat();
+  cat.meow();
 
-	var cat = Cat();
-	cat.color = "White";
-	cat.age = 6;
-	cat.eat();
-	cat.meow();
-
-	var animal = Animal();
-	animal.color = "brown";
-	animal.eat();
+  var animal = Animal();
+  animal.color = "brown";
+  animal.eat();
 }
 
 class Animal {
+  String? color;
 
-	String color;
-
-	void eat() {
-		print("Eat !");
-	}
+  void eat() {
+    print("Eat !");
+  }
 }
 
-class Dog extends Animal {      // Dog is Child class or sub class, Animal is super or parent class
+class Dog extends Animal {
+  // Dog is Child class or sub class, Animal is super or parent class
 
-	String breed;
+  String? breed;
 
-	void bark() {
-		print("Bark !");
-	}
+  void bark() {
+    print("Bark !");
+  }
 }
 
-class Cat extends Animal {      // Cat is Child class or sub class, Animal is super or parent class
+class Cat extends Animal {
+  // Cat is Child class or sub class, Animal is super or parent class
 
-	int age;
+  int? age = 0;
 
-	void meow() {
-		print("Meow !");
-	}
+  void meow() {
+    print("Meow !");
+  }
 }
--- 25_abstract_class_method.dart
+++ 25_abstract_class_method.dart
@@ -1,43 +1,37 @@
-
 // Objectives
 // 1. Abstract Method
 // 2. Abstract Class
 
 void main() {
-
 //	var shape = Shape();        // Error. Cannot instantiate Abstract Class
 
-	var rectangle = Rectangle();
-	rectangle.draw();
+  var rectangle = Rectangle();
+  rectangle.draw();
 
-	var circle = Circle();
-	circle.draw();
+  var circle = Circle();
+  circle.draw();
 }
 
 abstract class Shape {
+  // Define your Instance variable if needed
+  int x = 0;
+  int y = 0;
 
-	// Define your Instance variable if needed
-	int x;
-	int y;
+  void draw(); // Abstract Method
 
-	void draw();        // Abstract Method
-
-	void myNormalFunction() {
-		// Some code
-	}
+  void myNormalFunction() {
+    // Some code
+  }
 }
 
-
 class Rectangle extends Shape {
-
-	void draw() {
-		print("Drawing Rectangle.....");
-	}
+  void draw() {
+    print("Drawing Rectangle.....");
+  }
 }
 
 class Circle extends Shape {
-
-	void draw() {
-		print("Drawing Circle.....");
-	}
+  void draw() {
+    print("Drawing Circle.....");
+  }
 }
