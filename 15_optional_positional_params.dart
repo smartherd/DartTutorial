03c6d658df0f9547668d472de140cf57979dad8d
kach4n <luizkachan@gmail.com>
2023-06-15 14:28:22 -0300

    fix version errors

--- 15_optional_positional_params.dart
+++ 15_optional_positional_params.dart
@@ -1,28 +1,23 @@
-
 // 1. Required Parameters
 // 2. Optional Positional Parameters
 
 void main() {
+  printCities("New York", "New Delhi", "Sydney");
+  print("");
 
-	printCities("New York", "New Delhi", "Sydney");
-	print("");
-
-	printCountries("USA");  // You can skip the Optional Positional Parameters
-
+  printCountries("USA"); // You can skip the Optional Positional Parameters
 }
 
 // Required Parameters
 void printCities(String name1, String name2, String name3) {
-
-	print("Name 1 is $name1");
-	print("Name 2 is $name2");
-	print("Name 3 is $name3");
+  print("Name 1 is $name1");
+  print("Name 2 is $name2");
+  print("Name 3 is $name3");
 }
 
 // Optional Positional Parameters
-void printCountries(String name1, [String name2, String name3]) {
-
-	print("Name 1 is $name1");
-	print("Name 2 is $name2");
-	print("Name 3 is $name3");
+void printCountries(String name1, [name2, name3]) {
+  print("Name 1 is $name1");
+  print("Name 2 is $name2");
+  print("Name 3 is $name3");
 }
--- 20_constructors.dart
+++ 20_constructors.dart
@@ -1,4 +1,3 @@
-
 // Objectives
 // 1. Default Constructor
 // 2. Parameterized Constructor
@@ -6,48 +5,54 @@
 // 4. Constant Constructor
 
 void main() {
+  var student1 =
+      Student(23, "Peter"); // One Object, student1 is reference variable
+  print("${student1.id} and ${student1.name}");
 
-	var student1 = Student(23, "Peter"); 		// One Object, student1 is reference variable
-	print("${student1.id} and ${student1.name}");
+  student1.study();
+  student1.sleep();
 
-	student1.study();
-	student1.sleep();
+  var student2 =
+      Student(45, "Sam"); // One Object, student2 is reference variable
+  print("${student2.id} and ${student2.name}");
 
-	var student2 = Student(45, "Sam");		// One Object, student2 is reference variable
-	print("${student2.id} and ${student2.name}");
+  student2.study();
+  student2.sleep();
 
-	student2.study();
-	student2.sleep();
+  var student3 = Student
+      .myCustomConstructor(); // One object, student3 is a reference variable
+  student3.id = 54;
+  student3.name = "Rahul";
+  print("${student3.id} and ${student3.name}");
 
+  var student4 = Student.myAnotherNamedConstructor(87, "Paul");
+  print("${student4.id} and ${student4.name}");
 
-	var student3 = Student.myCustomConstructor();   // One object, student3 is a reference variable
-	student3.id = 54;
-	student3.name = "Rahul";
-	print("${student3.id} and ${student3.name}");
-
-
-	var student4 = Student.myAnotherNamedConstructor(87, "Paul");
-	print("${student4.id} and ${student4.name}");
+  var student5 = Student.myCustomConstructor();
+  print("${student5.id} and ${student5.name}");
 }
 
 // Define states (properties) and behavior of a Student
 class Student {
-	int id = -1;
-	String name;
+  int id = -1;
+  String? name = 'NonName';
 
-	Student(this.id, this.name);    // Parameterised Constructor
+  Student(this.id, this.name); // Parameterised Constructor
 
-	Student.myCustomConstructor() {                 // Named Constructor
-		print("This is my custom constructor");
-	}
+  Student.myCustomConstructor() {
+    // Named Constructor
+    this.id;
+    this.name;
+    print("This is my custom constructor");
+  }
 
-	Student.myAnotherNamedConstructor(this.id, this.name);  // Named Constructor
+  Student.myAnotherNamedConstructor(this.id, this.name); // Named Constructor
 
-	void study() {
-		print("${this.name} is now studying");
-	}
+  void study() {
+    print("${this.name} is now studying");
+  }
 
-	void sleep() {
-		print("${this.name} is now sleeping");
-	}
+  void sleep() {
+    print("${this.name} is now sleeping");
+  }
 }
