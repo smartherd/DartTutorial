6d4b82937d1671a026171cccba7aa2c8d1ce27db
kach4n <luizkachan@gmail.com>
2023-06-29 22:59:45 -0300

    fix warnings

--- 04_constants.dart
+++ 04_constants.dart
@@ -1,19 +1,18 @@
+// ignore_for_file: unused_local_variable
 
 void main() {
+  // final
+  final cityName = 'Mumbai';
+  //	name = 'Peter';     // Throws an error
 
-	// final
-	final cityName = 'Mumbai';
-	//	name = 'Peter';     // Throws an error
+  final String countryName = 'India';
 
-	final String countryName = 'India';
-
-	// const
-	const PI = 3.14;
-	const double gravity = 9.8;
+  // const
+  const PI = 3.14;
+  const double gravity = 9.8;
 }
 
 class Circle {
-
-	final color = 'Red';
-	static const PI = 3.14;
+  final color = 'Red';
+  static const PI = 3.14;
 }
--- 12_continue_keyword.dart
+++ 12_continue_keyword.dart
@@ -1,17 +1,17 @@
+// ignore_for_file: unused_label
 
 void main() {
+  // CONTINUE keyword
+  // Using Labels
 
-	// CONTINUE keyword
-	// Using Labels
-  
-	myLoop: for (int i = 1; i <= 3; i++) {
-
-		myInnerLoop: for (int j = 1; j <= 3; j++) {
-
-			if (i == 2 && j == 2) {
-				continue myLoop;
-			}
-			print("$i  $j");
-		}
-	}
+  myLoop:
+  for (int i = 1; i <= 3; i++) {
+    myInnerLoop:
+    for (int j = 1; j <= 3; j++) {
+      if (i == 2 && j == 2) {
+        continue myLoop;
+      }
+      print("$i  $j");
+    }
+  }
 }
